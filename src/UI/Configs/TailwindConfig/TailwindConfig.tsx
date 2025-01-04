20601ce50bdf81a248d4cb03dde18e4adfd7f4f3
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2025-01-04 07:56:48 +0530

    updated

---
+++ src/UI/Configs/TailwindConfig/TailwindConfig.tsx
@@ -0,0 +1,3 @@
+const TailwindConfig = "./node_modules/xtrixui/dist/**/*.{js,ts,jsx,tsx,mdx}";
+
+export { TailwindConfig };
--- src/UI/Configs/XtrixUITailwindConfig/XtrixUITailwindConfig.tsx
+++
@@ -1,4 +0,0 @@
-const XtrixUITailwindConfig =
-  "./node_modules/xtrixui/dist/**/*.{js,ts,jsx,tsx,mdx}";
-
-export { XtrixUITailwindConfig };
