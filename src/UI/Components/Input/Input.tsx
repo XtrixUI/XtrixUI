b825ce295c337dcaa718f97756a6c293dcb92eb2
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2025-02-26 22:56:30 +0530

    updated

--- src/UI/Components/Input/Input.tsx
+++ src/UI/Components/Input/Input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cfx(
-          "flex h-10 w-[16rem] rounded-2xl border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-[16rem] rounded-2xl border px-3 py-2 text-sm file:border-0 bg-white dark:bg-[#202020] file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
